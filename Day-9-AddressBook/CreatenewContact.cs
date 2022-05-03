9d76c69073b234824fd3ce149967d5d5e4da87e7
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-04 01:36:41 +0530

    Added: UC1_createNewContact

---
+++ Day-9-AddressBook/CreatenewContact.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9_AddressBook
+{
+    internal class CreatenewContact
+    {
+        //Init Local variables
+        String? FirstName;
+        String? LastName;
+        String? Email_Id;
+        String? PhoneNumber;
+        String? Address;
+        String? City;
+        String? State;
+        String? ZipCode;
+      
+
+        public void GetDetails()
+        {
+            Console.Write("\nEnter First Name: ");
+            FirstName = Console.ReadLine();
+
+            Console.Write("\nEnter Last Name: ");
+            LastName = Console.ReadLine();
+
+            Console.Write("\nEnter Address : ");
+            Address = Console.ReadLine();
+
+            Console.Write("\nEnter City : ");
+            City = Console.ReadLine();
+
+            Console.Write("\nEnter State : ");
+            State = Console.ReadLine();
+
+            Console.Write("\nEnter ZipCode: ");
+            ZipCode = Console.ReadLine();
+
+            Console.Write("\nEnter Phone Number: ");
+            PhoneNumber = Console.ReadLine();
+
+            Console.Write("\nEnter Email_Id: ");
+            Email_Id = Console.ReadLine();
+        }
+        public void  CreateDetails ()
+        {
+            
+            List<string> PersonList = new List<string>();
+            List<String> Listitem = new List<String> { "First Name", "Last Name", "Address", "City", "State", "Zip Code", "Email_Id"};
+            //Adding Details Entered by user details to list
+          
+            PersonList.Add(FirstName);
+            PersonList.Add(LastName);
+            PersonList.Add(Address);
+            PersonList.Add(City);
+            PersonList.Add(State);
+            PersonList.Add(ZipCode);
+            PersonList.Add(Email_Id);
+           
+            for (int i=0;i<PersonList.Count;i++)
+            {
+                for(int j=i;j<=Listitem.Count;j++)
+                {
+                    Console.WriteLine((i+1)+". "+Listitem[j]+" : "+PersonList[i]+"\n");
+                    break;
+                }
+
+              
+            }
+        }
+    }
+}
---
+++ Day-9-AddressBook/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day_9_AddressBook;
+
+Console.WriteLine("---- Welcome To Address Book Application ----\n");
+
+CreatenewContact obj = new CreatenewContact(); //creating obj of CreatenewContact class
+Console.WriteLine(">>>> Enter the Details :- ");
+obj.GetDetails(); //calling method which takes input from user
+Console.WriteLine("\n-------------------------------------------");
+Console.WriteLine("\n>>>> User Details :- \n");
+obj.CreateDetails(); //calling this method to store data in List and display on console
+
+
