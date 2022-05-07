fbb4b55b85f1e81cf1fe033b6c8a715a26027a93
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-07 23:44:03 +0530

    Added : UC_AddressBook

---
+++ Day-9-AddressBook/AddressBook.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9_AddressBook
+{
+    internal class AddressBook
+    {
+        public static List<Person> People = new List<Person>();
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Addresses { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string PinCode { get; set; }
+            public string PhoneNum { get; set; }
+            public string Email_Id { get; set; }
+        }
+        public static void GetCustomer()
+        {
+            Person person = new Person();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            person.Addresses = Console.ReadLine();
+
+            Console.Write("Enter City : ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter ZipCode: ");
+            person.PinCode = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNum = Console.ReadLine();
+
+            Console.Write("Enter EmailId: ");
+            person.Email_Id = Console.ReadLine();
+
+            People.Add(person);
+        }
+        public static void PrintCustomer(Person person)
+        {
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Address : " + person.Addresses);
+            Console.WriteLine("City : " + person.City);
+            Console.WriteLine("State : " + person.State);
+            Console.WriteLine("ZipCode : " + person.PinCode);
+            Console.WriteLine("Phone Number: " + person.PhoneNum);
+            Console.WriteLine("Email Id: " + person.Email_Id);
+            Console.WriteLine("-------------------------------------------");
+        }
+        public static void Modify()
+        {
+            if (People.Count != 0)
+            {
+                Console.WriteLine("Enter the First Name of contact to modify its Properties :");
+                string Modified = Console.ReadLine();
+                foreach (var person in People)
+                {
+                    if (person.FirstName.ToUpper() == Modified.ToUpper())
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Choose Operation to Perform : ");
+                            Console.WriteLine("1.Change First name ");
+                            Console.WriteLine("2.Change Last name ");
+                            Console.WriteLine("3.Change Address ");
+                            Console.WriteLine("4.Change City");
+                            Console.WriteLine("5.Change State ");
+                            Console.WriteLine("6.Change Pincode ");
+                            Console.WriteLine("7.Change Phone Number");
+                            Console.WriteLine("8.Exit ");
+                            int Check = Convert.ToInt32(Console.ReadLine());
+                            switch (Check)
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter the New First Name: ");
+                                    person.FirstName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the New Last Name: ");
+                                    person.LastName = Console.ReadLine();
+                                    break;
+                              
+                                case 3:
+                                    Console.WriteLine("Enter the New Address: ");
+                                    person.Addresses = Console.ReadLine();
+                                    break;
+
+                                case 4:
+                                    Console.WriteLine("Enter the New City: ");
+                                    person.City = Console.ReadLine();
+                                    break;
+
+                                case 5:
+                                    Console.WriteLine("Enter the New State: ");
+                                    person.State = Console.ReadLine();
+                                    break;
+
+                                case 6:
+                                    Console.WriteLine("Enter the New Pin Code: ");
+                                    person.PinCode = Console.ReadLine();
+                                    break;
+
+                                case 7:
+                                    Console.WriteLine("Enter the New Phone Number: ");
+                                    person.PhoneNum = Console.ReadLine();
+                                    break;
+
+                                case 8:
+                                    return;
+
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(">> There is No Such Contact with such First Name...");
+                    }
+
+                }
+
+
+            }
+        }
+
+        public static void ListingPeople()
+        {
+            if (People.Count == 0)
+            {
+                Console.WriteLine(">> Address Book is Empty...");
+                Console.ReadKey();
+                return;
+            }
+           
+            foreach (var person in People)
+            {
+                PrintCustomer(person);
+            }
+            Console.WriteLine("\n>> Press Enter to continue...\n");
+            Console.ReadKey();
+        }
+
+
+    }
+}
--- Day-9-AddressBook/Program.cs
+++ Day-9-AddressBook/Program.cs
@@ -4,11 +4,49 @@ using Day_9_AddressBook;
 
 Console.WriteLine("---- Welcome To Address Book Application ----\n");
 
-CreatenewContact obj = new CreatenewContact(); //creating obj of CreatenewContact class
-Console.WriteLine(">>>> Enter the Details :- ");
-obj.GetDetails(); //calling method which takes input from user
-Console.WriteLine("\n-------------------------------------------");
-Console.WriteLine("\n>>>> User Details :- \n");
-obj.CreateDetails(); //calling this method to store data in List and display on console
+int choice=0;
 
+while(choice!=5)
+{
+
+    Console.WriteLine("Choose the Operation:- \n1.Create Contact \n2.Add New Contact\n3.Modify Contact\n4.Display Address_Book\n5.Exit");
+    choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
+        {
+            case 1:
+                Console.WriteLine(">>>> Enter the Details :- ");
+                AddressBook.GetCustomer(); //calling method which takes input from user
+                Console.WriteLine("\nContact Added Successfully...\n");
+                break;
+
+            case 2:
+                Console.WriteLine(">>>> Enter the Details :- ");
+                AddressBook.GetCustomer(); //calling method which takes input from user
+                Console.WriteLine("\nConatact Added Successfully....\n");
+                break;
+
+            case 3:
+                Console.WriteLine(">>>> Modify Contact Details :-\n");
+                AddressBook.Modify();
+                break;
+
+
+
+            case 4:
+                Console.WriteLine(">>>> Address Book Details :-\n");
+                AddressBook.ListingPeople();
+                break;
+
+
+
+
+            default: 
+                Console.WriteLine(">> Please Enter valid choice.....\n");
+                break;     
+        
+
+        }
+
+}
 
